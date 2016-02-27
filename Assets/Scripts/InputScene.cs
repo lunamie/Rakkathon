e5650be5abeb8cb34444e72cff1d90cf68aa5dbe
HirokoFujisawa <hiropanda.cpp@gmail.com>
2016-02-28 03:48:15 +0900

    入力画面素材追加、UI差し替え、入力があるまで決定ボタン押せなくする処理追加

--- Assets/Scripts/InputScene.cs
+++ Assets/Scripts/InputScene.cs
@@ -5,10 +5,13 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class InputScene : MonoBehaviour {
-
-	[SerializeField] string[] NameList;
+	
 	[SerializeField] Dropdown NameDoropDown;	
 	[SerializeField] InputField Health;
+	[SerializeField] Button EnterButton;
+
+	private bool IsPossibleEntry = false;
+	private bool IsSettingName = false;
 
 	void Start () {
 		var nameMasterTable = new NameMasterTable ();
@@ -20,15 +23,21 @@ public class InputScene : MonoBehaviour {
 			nameList.options.Add (new Dropdown.OptionData(nameMasterTable.All[i].Name));
 		}
 		NameDoropDown.options = nameList.options;
+
+		// 初期値はなし
+		NameDoropDown.captionText.text = "";
 	}
 
 	void Update () {
-		
+		if (NameDoropDown.captionText.text != "" && Health.text != "") {
+			EnterButton.interactable = true;
+		} else if (NameDoropDown.captionText.text == "" || Health.text == "") {
+			EnterButton.interactable = false;
+		}
 	}
 
 	// 名前ドロップダウンリスト
 	public void OnValueChanged(int name){
-
 	}
 
 	// 決定ボタン
@@ -39,6 +48,6 @@ public class InputScene : MonoBehaviour {
 		PlayerPrefs.SetFloat (Const.GoalHealthKey,float.Parse(Health.text));
 
 		// ホームに遷移
-		SceneManager.LoadScene ("TitleScene");
+		SceneManager.LoadScene ("HomeScene");
 	}
 }
