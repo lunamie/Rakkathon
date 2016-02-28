f2e376b146666df622adced8d927b75ae7889595
HirokoFujisawa <hiropanda.cpp@gmail.com>
2016-02-28 14:06:32 +0900

    csv更新、マスタークラス更新

--- Assets/Scripts/Master/MessageMaster.cs
+++ Assets/Scripts/Master/MessageMaster.cs
@@ -11,5 +11,7 @@ public class MessageMaster : MasterBase
 {
 	public string Message { get; private set; }
 	public string SEName { get; private set; }
+	public int Type { get; private set; }
+	public int Delay { get; private set; }
 	public int Timing { get; private set; }
 }
