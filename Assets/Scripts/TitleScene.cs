763e4725872c89b672c9220857e49131ce007354
HirokoFujisawa <hiropanda.cpp@gmail.com>
2016-02-28 11:09:17 +0900

    起動時のボイスランダム再生追加

--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TitleScene : MonoBehaviour {
 
@@ -10,6 +11,26 @@ public class TitleScene : MonoBehaviour {
 		string name = PlayerPrefs.GetString (Const.NameKey);
 		if (name != "") {
 			IsEntry = true;
+		}
+
+		MessageMasterTable MessageTable = new MessageMasterTable();
+		MessageTable.Load();
+		// ここでランダムボイス再生
+		List<MessageMaster> randomSEs = MessageTable.All.FindAll(x => x.Timing == (int)Const.PlaytTiming.GameStart);
+		int result = Random.Range (0, randomSEs.Count);
+		// もし挨拶系のやつだったら時間を見て再生
+		if (randomSEs[result].SEName == "45" || randomSEs[result].SEName == "46" || randomSEs[result].SEName == "47") {
+			string se;
+			if (System.DateTime.Now.Hour < 12) {
+				se = "45";
+			} else if (System.DateTime.Now.Hour < 18) {
+				se = "46";
+			} else {
+				se = "47";
+			}
+			Audio.instance.PlaySE (string.Format(Const.SEPathFormat,se));
+		} else {
+			Audio.instance.PlaySE (string.Format(Const.SEPathFormat,randomSEs[result].SEName));
 		}
 	}
 
